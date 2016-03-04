ea802ff84dbc132dfde0778b196c0da9b7c2e5c9
Vatsal Manot <vatsal.manot@yahoo.com>
2016-03-04 23:49:44 +0530

    Update Swift._Strideable.swift

--- OpaqueSwift/Swift._Strideable.swift
+++ OpaqueSwift/Swift._Strideable.swift
@@ -8,12 +8,19 @@ public typealias Strideable2 = protocol<_Strideable, Strideable>
 
 public protocol _Strideable: _Comparable
 {
+    static var _Stride: Any.Type { get }
+    
     func _distanceTo(other: Any) -> Any?
     func _advancedBy(n: Any) -> Self?
 }
 
 extension _Strideable where Self: Strideable
 {
+    public static var _Stride: Any.Type
+    {
+        return Stride.self
+    }
+    
     public func _distanceTo(other: Any) -> Any?
     {
         return (other as? Self).map(distanceTo)
