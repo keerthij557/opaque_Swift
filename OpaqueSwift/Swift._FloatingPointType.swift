a6a6b402d1e2d8d5268f4e86036f5ec2d1528902
Vatsal Manot <vatsal.manot@yahoo.com>
2016-03-04 23:41:49 +0530

    Create Swift._FloatingPointType.swift

---
+++ OpaqueSwift/Swift._FloatingPointType.swift
@@ -0,0 +1,29 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+
+public protocol _FloatingPointType
+{
+    init(_ value: UInt8)
+    init(_ value: Int8)
+    init(_ value: UInt16)
+    init(_ value: Int16)
+    init(_ value: UInt32)
+    init(_ value: Int32)
+    init(_ value: UInt64)
+    init(_ value: Int64)
+    init(_ value: UInt)
+    init(_ value: Int)
+    
+    var floatingPointClass: FloatingPointClassification { get }
+    var isSignMinus: Bool { get }
+    var isNormal: Bool { get }
+    var isFinite: Bool { get }
+    var isZero: Bool { get }
+    var isSubnormal: Bool { get }
+    var isInfinite: Bool { get }
+    var isNaN: Bool { get }
+    var isSignaling: Bool { get }
+}
