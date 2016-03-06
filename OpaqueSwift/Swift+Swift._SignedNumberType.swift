1e9ce8cdfd21cffe264ae38145270a68f46e1fc0
Vatsal Manot <vatsal.manot@yahoo.com>
2016-03-06 11:04:58 +0530

    Create Swift+Swift._SignedNumberType.swift

---
+++ OpaqueSwift/Swift+Swift._SignedNumberType.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+
+extension Int: _SignedNumberType
+{
+    
+}
+
+extension Int8: _SignedNumberType
+{
+    
+}
+
+extension Int16: _SignedNumberType
+{
+    
+}
+
+extension Int32: _SignedNumberType
+{
+    
+}
+
+extension Int64: _SignedNumberType
+{
+    
+}
