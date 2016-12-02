5f7cef8509bd51cb0e76c601663aa612dd1ecf3a
Vatsal Manot <vatsal.manot@yahoo.com>
2016-12-03 01:54:42 +0530

    Create opaque_BidirectionalCollection.swift

---
+++ Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.opaque_BidirectionalCollection.swift
@@ -0,0 +1,12 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+
+public typealias BidirectionalCollection2 = opaque_BidirectionalCollection & BidirectionalCollection
+
+public protocol opaque_BidirectionalCollection: opaque_Collection
+{
+    
+}
