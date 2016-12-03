2e42ecf98aa304fb211aac334465e97a47dd850a
Vatsal Manot <vatsal.manot@yahoo.com>
2016-12-03 15:03:04 +0530

    Create & implement opaque_RandomAccessCollection

---
+++ Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.opaque_RandomAccessCollection.swift
@@ -0,0 +1,12 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+
+public typealias RandomAccessCollection2 = opaque_RandomAccessCollection & RandomAccessCollection
+
+public protocol opaque_RandomAccessCollection: opaque_BidirectionalCollection
+{
+    
+}
--- Targets/Framework (Common)/Files/Module Extensions/Protocol Implementations/Swift/Swift+Swift.opaque_Collection.swift
+++ Targets/Framework (Common)/Files/Module Extensions/Protocol Implementations/Swift/Swift+Swift.opaque_Collection.swift
@@ -9,27 +9,32 @@ extension AnyBidirectionalCollection: opaque_Collection
     
 }
 
-extension AnyRandomAccessCollection: opaque_Collection
+extension AnyRandomAccessCollection: opaque_Collection, opaque_RandomAccessCollection
 {
     
 }
 
-extension Array: opaque_MutableCollection
+extension Array: opaque_MutableCollection, opaque_RandomAccessCollection
 {
     
 }
 
-extension ArraySlice: opaque_MutableCollection
+extension ArraySlice: opaque_MutableCollection, opaque_RandomAccessCollection
 {
     
 }
 
-extension ContiguousArray: opaque_MutableCollection
+extension ContiguousArray: opaque_MutableCollection, opaque_RandomAccessCollection
 {
     
 }
 
 extension Dictionary: opaque_Collection
+{
+    
+}
+
+extension Set: opaque_Collection
 {
     
 }
