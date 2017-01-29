c3522427044b6367e993ff784c888f9c628514f8
Vatsal Manot <vatsal.manot@yahoo.com>
2017-01-29 20:52:32 +0530

    Rename project

---
+++ Targets/Framework (Common)/Files/Module Extensions/Protocol Implementations/Swift/Swift+Swift.opaque_BidirectionalCollection.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+
+extension UnsafeBufferPointer: opaque_BidirectionalCollection
+{
+    
+}
+
+extension UnsafeMutableBufferPointer: opaque_BidirectionalCollection
+{
+    
+}
+
+extension UnsafeMutableRawBufferPointer: opaque_BidirectionalCollection
+{
+    
+}
+
+extension UnsafeRawBufferPointer: opaque_BidirectionalCollection
+{
+    
+}
