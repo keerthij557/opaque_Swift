b590e52f1c141c28c9cb93423ae517e4cd20961c
Vatsal Manot <vatsal.manot@yahoo.com>
2017-03-09 14:04:25 +0530

    Modify project

--- Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.opaque_Hashable.swift
+++ Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.opaque_Hashable.swift
@@ -11,7 +11,49 @@ public protocol opaque_Hashable: opaque_Equatable
     var hashValue: Int { get }
 }
 
+// MARK: - Helpers - 
+
 @_transparent public func == <T: opaque_Hashable>(lhs: T, rhs: T) -> Bool
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: opaque_Hashable & Sequence>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Equatable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: opaque_Hashable & Sequence>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Hashable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: Hashable2 & Sequence>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Equatable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: Hashable2 & Sequence>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Hashable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: opaque_Hashable & Collection>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Equatable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: opaque_Hashable & Collection>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Hashable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: Hashable2 & Collection>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Equatable
+{
+    return lhs.hashValue == rhs.hashValue
+}
+
+@_transparent public func == <T: Hashable2 & Collection>(lhs: T, rhs: T) -> Bool where T.Iterator.Element: Hashable
 {
     return lhs.hashValue == rhs.hashValue
 }
