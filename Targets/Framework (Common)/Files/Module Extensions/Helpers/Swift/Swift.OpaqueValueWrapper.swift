dd05f9e385157d5919a0692b7730603e60c055be
Vatsal Manot <vatsal.manot@yahoo.com>
2017-03-20 23:01:19 +0530

    Modify project

--- Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.OpaqueValueWrapper.swift
+++ Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.OpaqueValueWrapper.swift
@@ -32,3 +32,32 @@ public protocol OpaqueValueWrapper: opaque_OpaqueValueWrapper
     
     init(opaqueValue: OpaqueValue)
 }
+
+public protocol opaque_FallibleOpaqueValueWrapper: AnyProtocol
+{
+    var opaque_opaqueValue: Any { get }
+    
+    static func opaque_OpaqueValueWrapper_init(opaqueValue: Any) -> opaque_OpaqueValueWrapper??
+}
+
+extension opaque_FallibleOpaqueValueWrapper where Self: FallibleOpaqueValueWrapper
+{
+    public var opaque_opaqueValue: Any
+    {
+        return opaqueValue
+    }
+    
+    public static func opaque_FallibleOpaqueValueWrapper_init(opaqueValue: Any) -> opaque_FallibleOpaqueValueWrapper??
+    {
+        return (-?>opaqueValue).map(self.init(opaqueValue:))
+    }
+}
+
+public protocol FallibleOpaqueValueWrapper: opaque_FallibleOpaqueValueWrapper
+{
+    associatedtype OpaqueValue
+    
+    var opaqueValue: OpaqueValue { get }
+    
+    init?(opaqueValue: OpaqueValue)
+}
