08f70281bc499e3ed17b9fc32666667659f47982
Vatsal Manot <vatsal.manot@yahoo.com>
2017-03-25 02:21:03 +0530

    Modify project

--- Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.withoutActuallyEscaping.swift
+++ Targets/Framework (Common)/Files/Module Extensions/Helpers/Swift/Swift.withoutActuallyEscaping.swift
@@ -1,13 +1,8 @@
 //
-//  Swift.withoutActuallyEscaping.swift
-//  opaque_Swift
-//
-//  Created by Vatsal Manot on 25/03/17.
-//  Copyright © 2017 Vatsal Manot. All rights reserved.
+// Copyright (c) Vatsal Manot
 //
 
-import Foundation
-
+import Swift
 
 public func withoutActuallyEscaping<T, U>(_ closure: ((T) -> U)) -> ((T) -> U)
 {
